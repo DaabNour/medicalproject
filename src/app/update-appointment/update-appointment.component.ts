47f2bf4a9712ab52509e5cc78059e77b41aefa41
unknown <Lenovo@DESKTOP-N4314633>
2023-09-17 17:58:54 +0100

    seventh commit

--- src/app/update-appointment/update-appointment.component.ts
+++ src/app/update-appointment/update-appointment.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup , FormControl , FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-update-appointment',
   templateUrl: './update-appointment.component.html',
   styleUrls: ['./update-appointment.component.css']
 })
-export class UpdateAppointmentComponent {
+export class UpdateAppointmentComponent implements OnInit{
+  ngOnInit(): void {
+
+  }
+  updateappForm:FormGroup
+
+  constructor(private fbapp:FormBuilder) {
+    let appformControls ={
+      firstname : new FormControl(),
+      appointmentdate : new FormControl(),
+      appointmenttime : new FormControl()
+
+
+
+    }
+    this.updateappForm=this.fbapp.group(appformControls)
+  }
+  get firstname() { return this.updateappForm.get('firstname') }
+  get lastname() { return this.updateappForm.get('lastname') }
+  get phone() { return this.updateappForm.get('phone') }
+
+
+ 
+ 
 
 }
